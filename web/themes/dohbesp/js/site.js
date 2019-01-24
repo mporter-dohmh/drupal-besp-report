005bd538f1c03026fa0dab57de8e9f8a35949768
mporter-dohmh <34313630+mporter-dohmh@users.noreply.github.com>
2019-01-24 18:17:06 -0500

    Initial commit

---
+++ web/themes/dohbesp/js/site.js
@@ -0,0 +1,86 @@
+(function ($) {
+    
+    $(document).ready(function(){
+        
+        // Navigation Scroller
+        $(document).on('click', '.navbar-nav a', function () {
+            var hash = $(this).prop("hash");
+            $('html, body').stop().animate({
+                scrollTop: $(hash).offset().top
+            }, 1000);
+            return false;
+        });
+        
+        // Slick Slider
+        $('.slider-container-arrows').slick({
+            dots: true,
+            arrows: true,
+            infinite: true,
+            speed: 300,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            fade: true,
+            accessibility: true
+        });
+        $('.slider-container-autoplay').slick({
+            dots: false,
+            arrows: false,
+            infinite: true,
+            speed: 600,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            fade: true,
+            autoplay: true,
+            accessibility: true
+        });
+        $('.slider-container-arrows-autoplay').slick({
+            dots: true,
+            arrows: true,
+            infinite: true,
+            autoplay: true,
+            speed: 500,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            fade: true,
+            accessibility: true
+        });
+
+
+        
+        // Pass Index Values to Accordions for Collapse
+        $('.panel-group').each(function (index, value) { 
+                $(this).find('.panel-collapse').first().addClass("in");
+                //console.log('.panel' + index + ':' + $(this).attr('id')); 
+                $(this).find('.panel').each(function (index, value) {
+                    $(this).find('.panel-title a').attr("href",'#collapse'+index);
+                    $(this).find('.panel-collapse').attr("id", 'collapse'+index);
+                });
+        });
+    });
+                      
+    /*Drupal.behaviors.myBehavior = {
+        attach: function (context, settings) {
+        // Using once() to apply the myCustomBehaviour effect when you want to run just one function.
+    $('section', context).once('myCustomBehavior').addClass('processed');
+      
+      $('#slider .slider-container', context).once('slickSlider').slick({
+                dots: false,
+                speed: 5000,
+                fade: true,
+                autoplay: true,
+                cssEase: 'linear'
+            });
+
+    // Using once() with more complexity.
+    $('section', context).once('mySecondBehavior').each(function () {
+      if ($(this).visible()) {
+        $(this).css('background', 'green');
+      }
+      else {
+        $(this).css('background', 'yellow').show();
+      }
+    });
+  }
+  };*/
+
+})(jQuery);
